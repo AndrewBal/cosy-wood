88d27e0d1931eb232517008f9359a7527194bc24
AndrewBal <it.andrey.bal@gmail.com>
2020-06-03 16:05:40 +0300

    add-tabs

--- _dev/js/script.js
+++ _dev/js/script.js
@@ -1,19 +1,29 @@
 "use strict";
 $(document).ready(function () {
-  var mySwiper = new Swiper('.slider-product .swiper-container', {
+  var mySwiper = new Swiper(".slider-product .swiper-container", {
     speed: 400,
     spaceBetween: 20,
-    slidesPerView: 'auto', 
+    slidesPerView: "auto",
     loop: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
-});
+  });
+
+  $(".tab-button").click(function () {
+    $(".tab-button").removeClass("active");
+    $(this).addClass("active");
+    $(this).prev().removeClass("active");
+    $(".tabs-content").toggleClass("active");
+  });
+
+
 
 });
+
