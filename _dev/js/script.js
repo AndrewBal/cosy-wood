b965a096e16f736a12792eca38b4374c6b63464b
AndrewBal <it.andrey.bal@gmail.com>
2020-06-05 16:07:26 +0300

    client

--- _dev/js/script.js
+++ _dev/js/script.js
@@ -16,6 +16,35 @@ $(document).ready(function () {
     },
   });
 
+
+  var gallerySwiper = new Swiper(".gallery-section .swiper-container", {
+    speed: 400,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+      nextEl: ".gallery-section .swiper-button-next",
+      prevEl: ".gallery-section .swiper-button-prev",
+    },
+    pagination: {
+      el: ".gallery-section .swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+
+  var reviewSwiper = new Swiper(".reviews-section .swiper-container", {
+    speed: 400,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: false,
+    navigation: {
+      nextEl: ".reviews-section .swiper-button-next",
+      prevEl: ".reviews-section .swiper-button-prev",
+    }
+  });
+
   $(".tab-button").click(function () {
     $(".tab-button").removeClass("active");
     $(this).addClass("active");
